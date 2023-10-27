5221cb90f27d4897096b7b37af74719074b4cad9
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-10-27 09:57:04 +0530

    Updates

---
+++ Day-1/day-1.js
@@ -0,0 +1,40 @@
+// 1. Datatypes 
+
+// 1. String 
+// 2. Number 
+// 3. Boolean 
+
+
+// String 
+
+// syntax -> '',"",``
+// "my name i awdiz"
+// 'my age is 10 years'
+// `Hello`
+// "my name is swaraj"
+console.log("my name is swaraj")
+console.log('my name is swaraj')
+console.log(`my name is swaraj`)
+console.log(typeof("awdiz"))
+console.log(typeof('awdiz'))
+console.log(typeof(`awdiz`))
+console.log(typeof(`987654`))
+console.log(typeof("8765"))
+console.log(typeof ('8887766'))
+console.log(15 + 15)
+console.log("15" + "15")
+
+
+// Number
+
+// syntax 
+
+console.log(2)
+console.log(29876)
+console.log(29876789)
+console.log(typeof(98765))
+console.log(typeof('98765'))
+
+console.log(1+1)
+console.log('1'+"1")
+
