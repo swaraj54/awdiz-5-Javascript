61dccc108f4a889db0183712c859e23a2119fa73
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-10-28 14:47:24 +0530

    Updates

--- Day-1/day-1.js
+++ Day-1/day-1.js
@@ -1,17 +1,17 @@
-// 1. Datatypes 
+1. Datatypes 
 
-// 1. String 
-// 2. Number 
-// 3. Boolean 
+1. String 
+2. Number 
+3. Boolean 
 
 
-// String 
+String 
 
-// syntax -> '',"",``
-// "my name i awdiz"
-// 'my age is 10 years'
-// `Hello`
-// "my name is swaraj"
+syntax -> '',"",``
+"my name i awdiz"
+'my age is 10 years'
+`Hello`
+"my name is swaraj"
 console.log("my name is swaraj")
 console.log('my name is swaraj')
 console.log(`my name is swaraj`)
@@ -21,8 +21,8 @@ console.log(typeof(`awdiz`))
 console.log(typeof(`987654`))
 console.log(typeof("8765"))
 console.log(typeof ('8887766'))
-console.log(15 + 15)
-console.log("15" + "15")
+console.log(15 + 15) // 30
+console.log("15" + "15") // 1515
 
 
 // Number
@@ -32,9 +32,9 @@ console.log("15" + "15")
 console.log(2)
 console.log(29876)
 console.log(29876789)
-console.log(typeof(98765))
-console.log(typeof('98765'))
+console.log(typeof(98765)) // number
+console.log(typeof('98765')) /// string
 
-console.log(1+1)
-console.log('1'+"1")
+console.log(1+1) // 2
+console.log('1'+"1") // 11
 
---
+++ Day-3/day-3.js
@@ -0,0 +1,118 @@
+console.log("awdiz")
+console.log(typeof ("awdiz"))
+console.log(typeof ("123"))
+console.log(typeof (1234))
+console.log("123" + "123")
+console.log(123 + 123)
+
+// swaraj()
+
+// 1. Pre defined function
+// 2. User created function 
+
+function add(num1, num2) {
+    return num1 + num2
+}
+add(2,3)
+
+
+
+// Boolean 
+
+// true , false 
+console.log(true)
+console.log(typeof (true))
+console.log(false)
+console.log(typeof (false))
+console.log(typeof('false'))
+
+
+// undefined
+
+// varible 
+// var 
+// const 
+// let 
+
+
+// var 
+
+// syntax 
+var name = "Awdiz"
+console.log(name)
+console.log(typeof(name))
+
+var myStudentsCount = 20;
+console.log(myStudentsCount)
+console.log(typeof(myStudentsCount))
+
+var kuchbhi = true;
+console.log(kuchbhi)
+
+var number1 = 50;
+var number2 = 100;
+console.log(number1, number2)
+console.log(number1 + number2)
+
+// Arthmetic operations 
+
+// + - * / %
+
+// var num1 = 10;
+// var num2 = 20;
+
+console.log(30 % 2)
+console.log(31 % 2)
+console.log(123456789 % 2)
+console.log(2 == 21)
+console.log(40 % 2 == 0)
+
+
+// 20 / 2 = 10 
+// 20 % 2 = 0
+
+//       ___ 10  = Quiotient
+//    2 / 20
+//        20
+//         0 = remainder
+
+
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+
+
+
+
+// Comparison Operator  
+//  == , === 
+
+console.log(2 == 2)
+console.log("2" == 2) //  check only data
+console.log("2" === 2) // check data and datatypes
+console.log("40" == 40  )
+console.log("400" === 400)
+
+
+// Logical operator 
+
+// && , ||
+
+// && - all conditions must be true then it'll return true otherwise false
+
+// || - if even one condition found true then it'll true , and when all
+//  conditions are false then then it'll return false
+
+var userAge = 124;
+
+
+console.log(userAge > 91 && userAge == 24 && userAge < 25 ) 
+console.log(userAge < 19 || userAge < 90 || userAge < 124)
+// console.log()
+
+console.log( 2 > 1)
+console.log(4 < 2)
+console.log( 6 >= 6)
+console.log(10 <= 8 )
